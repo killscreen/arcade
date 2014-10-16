6403085e53d6f9d292c847de7001cc307b0a658c
Killscreen <killscreen@users.noreply.github.com>
2014-10-15 17:38:30 -0700

    [Main] Read current game from query string

--- arcade/src/Main.js
+++ arcade/src/Main.js
@@ -2,6 +2,7 @@
 
 define(
     [
+        'require',        
         './Screen',
         './Display',
         './Driver',
@@ -9,25 +10,30 @@ define(
         './Game',
         './Clock',
         './Keyboard',
-        './Controller',
-        'games/paddles/Paddles'
+        './Controller'
     ],
-    function (Screen, Display, Driver, Engine, Game, Clock, Keyboard, Controller, Paddles) {
+    function (require, Screen, Display, Driver, Engine, Game, Clock, Keyboard, Controller) {
         "use strict";
         
-        var canvas = document.getElementById('screen'),
-            screen = new Screen(canvas),
-            display = new Display(screen),
-            keyboard = new Keyboard(document),
-            controller = new Controller(keyboard),
-            engine = new Engine([
-                new Clock(),
-                display,
-                controller,
-                new Game(Paddles)
-            ]),
-            driver = new Driver(engine, window, 20);
+        var scene = window.location.search.substring(1) || "Paddles";
 
-        driver.start();
+        function run(Scene) {
+            var canvas = document.getElementById('screen'),
+                screen = new Screen(canvas),
+                display = new Display(screen),
+                keyboard = new Keyboard(document),
+                controller = new Controller(keyboard),
+                engine = new Engine([
+                    new Clock(),
+                    display,
+                    controller,
+                    new Game(Scene)
+                ]),
+                driver = new Driver(engine, window, 20);
+
+            driver.start();
+        }
+
+        require(['games/' + scene.toLowerCase() + '/' + scene], run); 
     }
 );
