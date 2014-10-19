1cd8346de072347797ab7df45120230f7e726aed
Killscreen <killscreen@users.noreply.github.com>
2014-10-19 11:29:01 -0700

    [Bricks] Add checkerboard level

--- arcade/src/games/bricks/Level.js
+++ arcade/src/games/bricks/Level.js
@@ -22,7 +22,7 @@ define(
 
                     return bricks;
                 },
-                function (difficulty) {
+                function pyramid(difficulty) {
                     var row = 0,
                         count = Math.min(3 + difficulty * 5, ROWS),
                         scale = Math.max(difficulty - ROWS / 4 + 7, 3),
@@ -34,6 +34,27 @@ define(
                     for (row = 0; row < count; row = row + 1) {
                         for (x = row * offset; x + step <= 1 - row * offset + step / 2; x = x + step) {
                             bricks.push([ x, step, row ]);
+                        }
+                    }
+
+                    return bricks;
+                },
+                function checkers(difficulty) {
+                    var row = 0,
+                        count = Math.min(3 + difficulty, ROWS),
+                        scale = Math.round(Math.max(difficulty - ROWS + 7, 3) * 1.5),
+                        step = 1 / scale,
+                        x = 0,
+                        counter = 0,
+                        bricks = [];
+                        
+                    for (row = 0; row < count; row = row + 1) {
+                        counter = 0;
+                        for (x = 0; x + step <= 1 + step / 2; x = x + step) {
+                            if (counter % 2 === row % 2) {
+                                bricks.push([ x, step, row ]);
+                            }
+                            counter = counter + 1;
                         }
                     }
 
